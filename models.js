3f10ce1ec9f82f77a3f674eb23480f5cde21afd0
Chalon Lubin <chalonlubin1@gmail.com>
2023-03-04 16:09:42 -0800

    Add more class functions to seperate concerns further

--- models.js
+++ models.js
@@ -1,15 +1,147 @@
-class Cards {
-
-}
 class Game {
+  constructor(size) {
+    /** Construct each Game instance from:
+     *  - size: integer
+     *  - deck: array of images
+     */
+    try {
+      Number(size)
+    } catch(e){
+      console.error("Enter a valid number");
+    }
+    this.size = Math.floor(size);
+    this.deck = {};
+    this.disable = false;
+    this.matches = 0
+    this.moves = 0;
+    this.flipped = 0;
+    this.first = ""
+    this.second = ""
+  }
 
-}
-class Match {
+  /** Creates an array of count length containing randomly generated values.  */
+  generateArray() {
+    return Array.from({ length: this.size }, () => _.random(MIN, MAX));
+  }
 
+  /**
+   * Gets images from API to use for card game.
+   *
+   * Returns: Array of url linking to jpeg images.
+   */
+  async getItems() {
+    try {
+      let response = await axios.get(
+        `${BASE_URL}/${this.generateArray(this.size / 2)}`
+      );
+      for (let item of response.data) {
+        let { name, image } = item;
+        this.deck[_.camelCase(name)] = image;
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  flipCard(card) {
+    if (card.classList.contains("front")) {
+      card.classList.remove("front");
+      card.classList.add("clicked");
+      card.style.backgroundImage = `url(${this.deck[card.classList[0]]})`;
+      card.removeEventListener("click", this.flipCard);
+      if (!this.first) {
+        this.first = card.classList[0]
+      } else if (!this.second) {
+        this.second = deck.flipCard(evt.target)
+      }
+    }
+    this.moves += 1;
+    this.flipped += 1;
+    }
+  }
+
+  unflipCard(card) {
+    card.classList.remove("clicked");
+    card.classList.add("shake");
+    card.style.removeProperty("background-image");
+    card.classList.add("front");
+    setTimeout(() => {
+      card.classList.remove("shake");
+      card.addEventListener("click", this.flipCard);
+    }, 500);
+    // this.disable = false;
+  }
 }
+
 class Timer {
+  constructor(startBtn, timerText) {
+    this.startBtn = startBtn;
+    this.timerText = timerText;
+    this.int = null;
+    this.milliseconds = 0, this.seconds = 0, this.minutes = 0, this.hours = 0;
+  }
 
-}
+//   startTimer() {
+//     this.startBtn.addEventListener("click", () => {
+//       if (this.int !== 0) {
+//         clearInterval(this.int);
+//       }
+//       this.int = setInterval(this.displayTimer, 10);
+//       this.startBtn.classList.remove("retry");
+//       this.startBtn.classList.remove("hidden");
+//       // disable = false;
+//       // if (musicPlaying === false) playStart();
+//     });
+//   }
+
+//   resetTimer() {
+//     clearInterval(this.int);
+//     [this.milliseconds, this.seconds, this.minutes] = [0, 0, 0];
+//     this.timerText.innerHTML = "00 : 00 ";
+//   }
+
+//   displayTimer() {
+//     this.milliseconds += 10;
+//     if (this.milliseconds == 1000) {
+//       this.milliseconds = 0;
+//       this.seconds++;
+//       if (this.seconds == 60) {
+//         this.seconds = 0;
+//         this.minutes++;
+//         if (this.minutes == 60) {
+//           this.minutes = 0;
+//           this.hours++;
+//         }
+//       }
+//     }
+
+//     let m = this.minutes < 10 ? "0" + this.minutes : this.minutes;
+//     let s = this.seconds < 10 ? "0" + this.seconds : this.seconds;
+//     this.timerText.innerHTML = `${m}:${s}`;
+//   }
+// }
+
 class Effect {
-
+  constructor(start, match, win) {
+    this.start = start;
+    this.match = match;
+    this.win = win;
+  }
+  playStart() {
+    let audioStart = new Audio(this.start);
+    audioStart.loop = true;
+    audioStart.volume = 0.2;
+    audioStart.play();
+  }
+  playMatch() {
+    let audio = new Audio(this.match);
+    audio.loop = false;
+    audio.play();
+  }
+  playWin() {
+    let audioWin = new Audio(this.win);
+    audioWin.loop = false;
+    audioWin.volume = 0.5;
+    audioWin.play();
+  }
 }
--- ui.js
+++ ui.js
@@ -3,31 +3,69 @@
 const BASE_URL = "https://rickandmortyapi.com/api/character";
 const MAX = 150;
 const MIN = 1;
+const DEFAULT_MATCHES = 8;
 
-const $START_BUTTON = $("#start")
-const $BOARD = $("#board")
+const $START_BUTTON = $("#start");
+const $GAMEBOARD = $("#game");
 const $TIME_DISPLAY = $("#timer-display");
 
+let deck;
 
-//******* Probably can go in cards class */
-
-/** Creates an array of count length containing randomly generated values.  */
-function generateArray(count) {
-  return Array.from({ length: count }, () => _.random(MIN, MAX));
+function startGame() {
+  createBoard();
+  // start the time
 }
 
-/**
- * Pulls images from API to use for card game.
- *
- * Returns: Array of url linking to jpeg images.
-*/
-async function getImages(num) {
-  try {
-    let randomArray = generateArray(num);
-    let response = await axios.get(`${BASE_URL}/${randomArray}`);
-    return response.data.map((card) => card["image"]);
-  } catch (e) {
-    error.log(e);
+async function createBoard() {
+  deck = new Game(DEFAULT_MATCHES);
+  await deck.getItems();
+  let names = [];
+
+  for (let item in deck.deck) {
+    names.push(item);
+    names.push(item);
+  }
+  names = _.shuffle(names);
+
+  for (let name of names) {
+    let curCard = document.createElement("div");
+    curCard.classList.add(name, "aCard", "front");
+    $GAMEBOARD.append(curCard);
+    curCard.addEventListener("click", handleClick);
   }
 }
 
+function handleClick(evt) {
+  evt.preventDefault();
+
+  if (!deck.disable && curCard.classList[2] === "front") {
+    deck.flipCard(evt.target);
+  }
+  if (deck.flipped === 2) {
+    checkMatch();
+  }
+}
+
+function checkMatch() {
+  let $matches = $(".clicked");
+  if (deck.first === deck.second) {
+    deck.matches += 1;
+    document.querySelector(
+      "#current-score"
+    ).innerText = `Your Matches: ${deck.matches}`;
+    for (let match of $matches) {
+      match.classList.add("matched");
+      match.classList.remove("clicked");
+      match.removeEventListener("click", handleClick);
+    }
+  } else {
+    $matches.forEach(match => deck.unflipCard(match))
+  }
+}
+
+// function startTimer() {
+//   let timer = new Timer($START_BUTTON, $TIME_DISPLAY);
+//   timer.startTimer()
+// }
+
+$START_BUTTON.on("click", startGame);
